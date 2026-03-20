6f008f1f2c4748c87bd495411d8a5ccb5dd132d0
Santhosh Ramesh <155134705+SanthoshRamesh007@users.noreply.github.com>
2026-03-20 22:01:33 +0530

    Remove poweranalytics-desktop folder

--- poweranalytics-desktop/components/SmartNarrative.jsx
+++
@@ -1,8 +0,0 @@
-
-import React from 'react';
-
-const SmartNarrative = () => {
-    return null;
-};
-
-export default SmartNarrative;
--- poweranalytics-desktop/services/geminiService.js
+++
@@ -1,4 +0,0 @@
-
-// This file is deprecated as per the request to avoid using AI.
-export const getSmartNarrative = async () => [];
-export const queryData = async () => "";
